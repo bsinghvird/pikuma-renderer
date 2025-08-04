9d71b095cd2c5b8a7e8ba4141b9b6c3c0ff630dc
bsinghvird <39363281+bsinghvird@users.noreply.github.com>
2025-08-03 23:26:59 -0700

    Add project files.

---
+++ renderer/Main.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include "display.h"
+#include "vector.h"
+
+
+bool is_running = false;
+
+void setup(void)
+{
+	//allocate the required memory in bytes to hold the color buffer
+	color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
+
+	//creatning an SDL texture that is used to display the color buffer
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+		);
+
+}
+
+void process_input(void)
+{
+	SDL_Event event;
+	SDL_PollEvent(&event);
+
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			is_running = false;
+			break;
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;
+	}
+
+}
+
+void update(void)
+{
+
+}
+
+
+void render()
+{
+	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	draw_grid();
+
+	draw_pixel(20, 20, 0xFFFFFF00);
+	draw_rect(300, 200, 300, 150, 0xFF003300);
+	
+
+	render_color_buffer();
+
+	clear_color_buffer(0xFF000000);
+
+	SDL_RenderPresent(renderer);
+}
+
+int main(int argc, char* args[])
+{
+	is_running = initialize_window();
+
+	setup();
+
+
+	vect3_t myvector = { 2.0,3.0,1.0 };
+
+
+	while (is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
+
+	destroy_window();
+
+	return 0;
+}
---
+++ renderer/vector.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+typedef struct
+{
+	float x; 
+	float y;
+} vect2_t;
+
+typedef struct
+{
+	float x;
+	float y;
+	float z;
+} vect3_t;
+
+
+
+
+#endif // !VECTOR_H
+
